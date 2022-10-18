4987f9e8f5996ba861ba874e8a0d219c1af8e35f
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2022-10-18 00:13:23 -0300

    feat(views): :sparkles: scaffold project views

---
+++ src/views/AccountsView.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <FButton
+            text="Adicionar Conta"
+            @click="goToCreateAccount"></FButton>
+    </header>
+    <ul>
+        <li
+            v-for="account in accountsStore.unfinishedAccounts"
+            :key="account.id">
+            {{ account }}
+        </li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+    import { useAccountsStore } from '@/stores/account';
+    import { useRouter } from 'vue-router';
+    import FButton from '../components/FButton.vue';
+
+    const router = useRouter();
+    const accountsStore = useAccountsStore();
+
+    function goToCreateAccount() {
+        router.push({ name: 'create-account' });
+    }
+</script>
+
+<style scoped></style>
---
+++ src/views/CreateAccountView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-full">
+        <FInput
+            v-model="accountName"
+            label="Nome da conta"></FInput>
+
+        <FButton text="Criar conta"></FButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import FInput from '../components/FInput.vue';
+    import FButton from '../components/FButton.vue';
+
+    const accountName = ref('');
+</script>
+
+<style scoped></style>
