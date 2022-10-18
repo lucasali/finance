2e340a2d034460c43d511fee3b0ffd7c841bf4c3
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2022-10-18 00:13:58 -0300

    feat(layout): :sparkles: make default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+        <FHeader></FHeader>
+
+        <div class="flex flex-1">
+            <FSidebar></FSidebar>
+
+            <main class="flex flex-1 p-4">
+                <RouterView></RouterView>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { RouterView } from 'vue-router';
+    import FHeader from '../components/FHeader.vue';
+    import FSidebar from '../components/FSidebar.vue';
+</script>
+
+<style scoped></style>
